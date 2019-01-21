ec2cb3de924065b7182661c7daafe36707f636ca
Moses Surumen <moses.susrumen@gmail.com>
2019-01-20 18:07:45 -0800

    Updates

---
+++ precache-manifest.3bb0acd91aaf3a6788626b644ad9c2e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "381e3e15d3be97bab48090cc25a4c38d",
+    "url": "/projects.surumen.com/static/media/sound.381e3e15.svg"
+  },
+  {
+    "revision": "b4d89db402f4035bc16c81ceeda0a175",
+    "url": "/projects.surumen.com/static/media/mobile.b4d89db4.png"
+  },
+  {
+    "revision": "649b87e5646dcd162065cb4b7f0cff7b",
+    "url": "/projects.surumen.com/static/media/mobile.649b87e5.webp"
+  },
+  {
+    "revision": "84f2c050982ad67a0a96545434783f25",
+    "url": "/projects.surumen.com/static/media/genres.84f2c050.png"
+  },
+  {
+    "revision": "fd34a51e3a9d8781bbf2",
+    "url": "/projects.surumen.com/static/js/runtime~main.fd34a51e.js"
+  },
+  {
+    "revision": "a3ff23ef58123af2fa3c",
+    "url": "/projects.surumen.com/static/js/main.a3ff23ef.chunk.js"
+  },
+  {
+    "revision": "35386e9cb81c0d606de0",
+    "url": "/projects.surumen.com/static/js/1.35386e9c.chunk.js"
+  },
+  {
+    "revision": "a3ff23ef58123af2fa3c",
+    "url": "/projects.surumen.com/static/css/main.c4ec45b8.chunk.css"
+  },
+  {
+    "revision": "aa9ad403f203e7467bf5e97ea7baacd0",
+    "url": "/projects.surumen.com/index.html"
+  }
+];
