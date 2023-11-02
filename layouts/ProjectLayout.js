fff6aa55a6c1c5c34ac7ad2c2b6a2a45c7fc5b3b
Moses Surumen <moses.susrumen@gmail.com>
2023-11-01 22:43:18 -0700

    modal search

--- layouts/ProjectLayout.js
+++ layouts/ProjectLayout.js
@@ -11,11 +11,11 @@ const ProjectLayout = (props) => {
         <div className='d-flex flex-column flex-lg-row h-lg-100'>
             <nav className="flex-none navbar navbar-vertical navbar-expand-lg navbar-light bg-transparent show vh-lg-100 px-0 py-0">
                 <Sidebar
-                    className={'bg-ironside-gray-600 py-2 py-md-1'}
+                    className={'bg-charcoal-gray-600 py-2 py-md-1'}
                 />
             </nav>
             <div className='flex-lg-fill overflow-x-auto vstack vh-lg-100 position-relative'>
-                <div className='d-none d-lg-flex py-3 bg-ironside-gray-200 border-bottom'>
+                <div className='d-none d-lg-flex py-3 bg-light border-bottom'>
                     <NavbarTop search={false}/>
                 </div>
                 <div className='flex-fill overflow-y-lg-auto scrollbar overflow-x-hidden bg-body'>
