21d15a49438d14c61b534979e49ddcc02cc43118
Moses Surumen <moses.susrumen@gmail.com>
2023-11-01 22:56:31 -0700

    modal search

--- layouts/ProjectLayout.js
+++ layouts/ProjectLayout.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 
 // import sub components
-import { Sidebar, NavbarTop } from '../app/widgets';
+import { Sidebar, NavbarTop } from 'app/widgets';
 
 const ProjectLayout = (props) => {
 
