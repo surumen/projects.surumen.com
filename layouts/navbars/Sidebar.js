eeac443a224840607d62af985e06b1839f0977aa
Moses Surumen <moses.susrumen@gmail.com>
2023-11-01 22:50:50 -0700

    modal search

--- layouts/navbars/Sidebar.js
+++ layouts/navbars/Sidebar.js
@@ -14,12 +14,8 @@ const Sidebar = (props) => {
 		<Fragment>
 			<Container className={`container-fluid px-3 px-md-4 px-lg-6 ${props.className}`}>
 				<Navbar.Brand href="/" className="navbar-brand d-inline-block py-lg-1 mb-lg-5">
-					<Image src="/images/icons/brain.svg" alt="" className={
-						`h-rem-8 h-rem-md-16 ${props.className ? 'd-none' : 'logo-logo-dark'}`
-					} />
-					<Image src="/images/icons/logo-light.svg" alt="" className={
-						`h-rem-8 h-rem-md-16 ${props.className ? props.className : 'logo-light'}`
-					} />
+					<Image src={props.className ? '/images/icons/logo-light.svg' : '/images/icons/brain.svg'} alt="" className='h-rem-8 h-rem-md-16 logo-dark' />
+					<Image src="/images/icons/logo-light.svg" alt="" className='h-rem-8 h-rem-md-16 logo-light' />
 				</Navbar.Brand>
 			</Container>
 		</Fragment>
