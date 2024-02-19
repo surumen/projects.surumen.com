1f12425a12c61c001ee567cb16755def31540c91
Moses Surumen <moses.susrumen@gmail.com>
2024-02-18 23:53:08 -0800

    data sets

--- app/data/world-cup-18/qf.js
+++ app/data/world-cup-18/qf.js
@@ -1,4 +1,12 @@
-import { RoundOf16_1 } from "./r16";
+import {
+    RoundOf16_M1,
+    RoundOf16_M2,
+    RoundOf16_M3,
+    RoundOf16_M4,
+    RoundOf16_M5,
+    RoundOf16_M6,
+    RoundOf16_M7, RoundOf16_M8
+} from "./r16";
 
 export const QuarterFinals1 = {
     '@id': 'QF1',
@@ -14,16 +22,20 @@ export const QuarterFinals1 = {
                 'name': 'Uruguay',
             },
             'score': { 'score': 0, 'notes': null },
-            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '14' }, 'rank': 1, 'displayName': '2nd place of A' }
+            'seed': {
+                'sourceGame': RoundOf16_M2,
+                'sourcePool': { '@ref': '14' }, 'rank': 1,
+                'displayName': 'Winner Round of 16 M2'
+            }
         },
         'visitor': {
             'team': { '@ref': 'FRANCE' },
             'score': { 'score': 2, 'notes': null },
             'seed': {
-                'sourceGame': RoundOf16_1,
+                'sourceGame': RoundOf16_M1,
                 'sourcePool': { '@ref': '12' },
                 'rank': 1,
-                'displayName': 'Winner Round of 16'
+                'displayName': 'Winner Round of 16 M1'
             }
         }
     }
@@ -43,12 +55,22 @@ export const QuarterFinals2 = {
                 'name': 'Brazil',
             },
             'score': { 'score': 1, 'notes': null },
-            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '14' }, 'rank': 1, 'displayName': '2nd place of A' }
+            'seed': {
+                'sourceGame': RoundOf16_M3,
+                'sourcePool': { '@ref': '14' },
+                'rank': 1,
+                'displayName': 'Winner Round of 16 M3'
+            }
         },
         'visitor': {
             'team': { '@ref': 'BELGIUM' },
             'score': { 'score': 2, 'notes': null },
-            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '12' }, 'rank': 1, 'displayName': '3rd place of B' }
+            'seed': {
+                'sourceGame': RoundOf16_M4,
+                'sourcePool': { '@ref': '12' },
+                'rank': 1,
+                'displayName': 'Winner Round of 16 M4'
+            }
         }
     }
 }
@@ -67,12 +89,22 @@ export const QuarterFinals3 = {
                 'name': 'Russia',
             },
             'score': { 'score': 3, 'notes': '(3)' },
-            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '14' }, 'rank': 1, 'displayName': '2nd place of A' }
+            'seed': {
+                'sourceGame': RoundOf16_M5,
+                'sourcePool': { '@ref': '14' },
+                'rank': 1,
+                'displayName': 'Winner Round of 16 M5'
+            }
         },
         'visitor': {
             'team': { '@ref': 'CROATIA' },
             'score': { 'score': 4, 'notes': '(4)' },
-            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '12' }, 'rank': 1, 'displayName': '3rd place of B' }
+            'seed': {
+                'sourceGame': RoundOf16_M6,
+                'sourcePool': { '@ref': '12' },
+                'rank': 1,
+                'displayName': 'Winner Round of 16 M6'
+            }
         }
     }
 }
@@ -80,7 +112,7 @@ export const QuarterFinals3 = {
 
 export const QuarterFinals4 = {
     '@id': 'QF4',
-    'id': 'b669ccad-41-5df82458',
+    'id': 'b669ccad-41-5df82458-poj07wy',
     'name': 'QF4',
     'scheduled': 1499547600000,
     'court': { '@ref': '16' },
@@ -92,12 +124,22 @@ export const QuarterFinals4 = {
                 'name': 'Sweden',
             },
             'score': { 'score': 0, 'notes': null },
-            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '14' }, 'rank': 1, 'displayName': '2nd place of A' }
+            'seed': {
+                'sourceGame': RoundOf16_M7,
+                'sourcePool': { '@ref': '14' },
+                'rank': 1,
+                'displayName': 'Winner Round of 16 M7'
+            }
         },
         'visitor': {
             'team': { '@ref': 'ENGLAND' },
             'score': { 'score': 2, 'notes': null },
-            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '12' }, 'rank': 1, 'displayName': '3rd place of B' }
+            'seed': {
+                'sourceGame': RoundOf16_M8,
+                'sourcePool': { '@ref': '12' },
+                'rank': 1,
+                'displayName': 'Winner Round of 16 M8'
+            }
         }
     }
 }
--- app/data/world-cup-18/r16.js
+++ app/data/world-cup-18/r16.js
@@ -1,7 +1,7 @@
 
 
-export const RoundOf16_1 = {
-    '@id': 'R16_1',
+export const RoundOf16_M1 = {
+    '@id': 'R16_M1',
     'id': 'b6e8651-91-5df82458',
     'name': 'Round of 16',
     'scheduled': 1499547600000,
@@ -19,6 +19,174 @@ export const RoundOf16_1 = {
                 'name': 'Argentina',
             },
             'score': { 'score': 3, 'notes': null },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '12' }, 'rank': 1, 'displayName': '3rd place of B' }
+        }
+    }
+}
+
+export const RoundOf16_M2 = {
+    '@id': 'R16_M2',
+    'id': 'b6e8651-91-5df82458-dwed-ww4',
+    'name': 'Round of 16',
+    'scheduled': 1499547600000,
+    'court': { '@ref': '16' },
+    'sides': {
+        'home': {
+            'team': { '@ref': 'URUGUAY' },
+            'score': { 'score': 2, 'notes': null },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '14' }, 'rank': 1, 'displayName': '2nd place of A' }
+        },
+        'visitor': {
+            'team': {
+                '@id': 'PORTUGAL',
+                'id': 'portugal-id',
+                'name': 'Portugal',
+            },
+            'score': { 'score': 1, 'notes': null },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '12' }, 'rank': 1, 'displayName': '3rd place of B' }
+        }
+    }
+}
+
+export const RoundOf16_M3 = {
+    '@id': 'R16_M3',
+    'id': 'b6e8651-91-5df82458-dwe4',
+    'name': 'Round of 16',
+    'scheduled': 1499547600000,
+    'court': { '@ref': '16' },
+    'sides': {
+        'home': {
+            'team': { '@ref': 'BRAZIL' },
+            'score': { 'score': 2, 'notes': null },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '14' }, 'rank': 1, 'displayName': '2nd place of A' }
+        },
+        'visitor': {
+            'team': {
+                '@id': 'MEXICO',
+                'id': 'mexico-id',
+                'name': 'Mexico',
+            },
+            'score': { 'score': 0, 'notes': null },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '12' }, 'rank': 1, 'displayName': '3rd place of B' }
+        }
+    }
+}
+
+export const RoundOf16_M4 = {
+    '@id': 'R16_M4',
+    'id': 'b6e8651-91-5df82458-dwe4-89qedn89',
+    'name': 'Round of 16',
+    'scheduled': 1499547600000,
+    'court': { '@ref': '16' },
+    'sides': {
+        'home': {
+            'team': { '@ref': 'BELGIUM' },
+            'score': { 'score': 3, 'notes': null },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '14' }, 'rank': 1, 'displayName': '2nd place of A' }
+        },
+        'visitor': {
+            'team': {
+                '@id': 'JAPAN',
+                'id': 'japan-id',
+                'name': 'Japan',
+            },
+            'score': { 'score': 2, 'notes': null },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '12' }, 'rank': 1, 'displayName': '3rd place of B' }
+        }
+    }
+}
+
+export const RoundOf16_M5 = {
+    '@id': 'R16_M5',
+    'id': 'b6e8651-91-5df82458-dwe4-89qedn89-0aj983',
+    'name': 'Round of 16',
+    'scheduled': 1499547600000,
+    'court': { '@ref': '16' },
+    'sides': {
+        'home': {
+            'team': {
+                '@id': 'SPAIN',
+                'id': 'spain-id',
+                'name': 'Spain',
+            },
+            'score': { 'score': 3, 'notes': '(3)' },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '14' }, 'rank': 1, 'displayName': '2nd place of A' }
+        },
+        'visitor': {
+            'team': { '@ref': 'RUSSIA' },
+            'score': { 'score': 4, 'notes': '(4)' },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '12' }, 'rank': 1, 'displayName': '3rd place of B' }
+        }
+    }
+}
+
+export const RoundOf16_M6 = {
+    '@id': 'R16_M6',
+    'id': 'b6e8651-91-5df82458-dwe4-89qed-d9edw89',
+    'name': 'Round of 16',
+    'scheduled': 1499547600000,
+    'court': { '@ref': '16' },
+    'sides': {
+        'home': {
+            'team': { '@ref': 'CROATIA' },
+            'score': { 'score': 3, 'notes': '(1)' },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '14' }, 'rank': 1, 'displayName': '2nd place of A' }
+        },
+        'visitor': {
+            'team': {
+                '@id': 'DENMARK',
+                'id': 'denmark-id',
+                'name': 'Denmark',
+            },
+            'score': { 'score': 2, 'notes': '(1)' },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '12' }, 'rank': 1, 'displayName': '3rd place of B' }
+        }
+    }
+}
+
+export const RoundOf16_M7 = {
+    '@id': 'R16_M7',
+    'id': 'b6e8651-91-5df8dwe4-89qed-d9edw89',
+    'name': 'Round of 16',
+    'scheduled': 1499547600000,
+    'court': { '@ref': '16' },
+    'sides': {
+        'home': {
+            'team': { '@ref': 'SWEDEN' },
+            'score': { 'score': 1, 'notes': null },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '14' }, 'rank': 1, 'displayName': '2nd place of A' }
+        },
+        'visitor': {
+            'team': {
+                '@id': 'SWITZERLAND',
+                'id': 'switzerland-id',
+                'name': 'Switzerland',
+            },
+            'score': { 'score': 0, 'notes': null },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '12' }, 'rank': 1, 'displayName': '3rd place of B' }
+        }
+    }
+}
+
+export const RoundOf16_M8 = {
+    '@id': 'R16_M8',
+    'id': 'b6e8651-91-5df82458-dwe4-89qed-w89',
+    'name': 'Round of 16',
+    'scheduled': 1499547600000,
+    'court': { '@ref': '16' },
+    'sides': {
+        'home': {
+            'team': {
+                '@id': 'COLOMBIA',
+                'id': 'colombia-id',
+                'name': 'Colombia',
+            },
+            'score': { 'score': 3, 'notes': '(1)' },
+            'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '14' }, 'rank': 1, 'displayName': '2nd place of A' }
+        },
+        'visitor': {
+            'team': { '@ref': 'ENGLAND' },
+            'score': { 'score': 4, 'notes': '(1)' },
             'seed': { 'sourceGame': null, 'sourcePool': { '@ref': '12' }, 'rank': 1, 'displayName': '3rd place of B' }
         }
     }
