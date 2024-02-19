e628e5fe878045180080199bdc3a6644767eb224
Moses Surumen <moses.susrumen@gmail.com>
2024-02-19 13:37:18 -0800

    typescript fixes

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  //zbasePath: '/projects.surumen.com',
-  webpack(config) {
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-  }
+  basePath: '/projects.surumen.com'
 }
 
 module.exports = nextConfig
