0b7b2ea949d6706f91bbe74db4884c246f42ac33
Moses Surumen <moses.susrumen@gmail.com>
2024-03-02 22:33:05 -0800

    projects

--- app/data/projects/AllProjectsData.ts
+++ app/data/projects/AllProjectsData.ts
@@ -6,27 +6,6 @@ export const AllProjectsData: Project[] = [
 		id: uuid(),
 		language: 'Python',
 		technologyAreas: ['Python', 'Recurrent Neural Networks'],
-		title: 'Predictive Analysis: Fantasy Football Players Power Index',
-		slug: 'fantasy-football-power-index',
-		shortDescription:
-			'Aliquam pulvinar eros a dictur vitae diam imperdiet, ornare turpis vequet elit nec, imperdiet lectuna liquam qs.',
-		status: 'Pending',
-		level: 'Advance',
-		duration: '2h 40m',
-		rating: 2.5,
-		completed: '2018',
-		description: `<p>We'll dive into GraphQL, the fundamentals of GraphQL. 
-        We're only gonna use the pieces of it that we need to build in Gatsby. 
-        We're not gonna be doing a deep dive into what GraphQL is or the language specifics. 
-        We're also gonna get into MDX. MDX is a way to write React components in your markdown.</p>
-        `,
-		contentType: 'blog',
-		content: ``
-	},
-	{
-		id: uuid(),
-		language: 'Python',
-		technologyAreas: ['Python', 'Recurrent Neural Networks'],
 		title: 'March Madness Brackets Assistant',
 		slug: 'march-madness-assistant',
 		shortDescription:
@@ -43,6 +22,27 @@ export const AllProjectsData: Project[] = [
         `,
 		component: 'tournament',
 		contentType: 'app',
+		content: ``
+	},
+	{
+		id: uuid(),
+		language: 'Python',
+		technologyAreas: ['Python', 'Recurrent Neural Networks'],
+		title: 'Predictive Analysis: Fantasy Football Players Power Index',
+		slug: 'fantasy-football-power-index',
+		shortDescription:
+			'Aliquam pulvinar eros a dictur vitae diam imperdiet, ornare turpis vequet elit nec, imperdiet lectuna liquam qs.',
+		status: 'Pending',
+		level: 'Advance',
+		duration: '2h 40m',
+		rating: 2.5,
+		completed: '2018',
+		description: `<p>We'll dive into GraphQL, the fundamentals of GraphQL. 
+        We're only gonna use the pieces of it that we need to build in Gatsby. 
+        We're not gonna be doing a deep dive into what GraphQL is or the language specifics. 
+        We're also gonna get into MDX. MDX is a way to write React components in your markdown.</p>
+        `,
+		contentType: 'blog',
 		content: ``
 	},
 ];
