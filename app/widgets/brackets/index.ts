a54beb88c478617692f5a5033f038e11745897bf
Moses Surumen <moses.susrumen@gmail.com>
2024-03-02 22:39:34 -0800

    tournaments project

--- app/widgets/brackets/index.ts
+++
@@ -1,9 +0,0 @@
-import Bracket from './components/Bracket';
-import BracketGame from './components/BracketGame';
-import BracketGenerator from './components/BracketGenerator';
-
-export {
-    Bracket,
-    BracketGame,
-    BracketGenerator
-};
