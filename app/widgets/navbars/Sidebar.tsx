378ebc8a830b8d705bae79c8a4a96efe31bfbaa4
Moses Surumen <moses.susrumen@gmail.com>
2025-05-11 09:51:04 -0700

    make responsive

--- app/widgets/navbars/Sidebar.tsx
+++ app/widgets/navbars/Sidebar.tsx
@@ -20,8 +20,7 @@ const Sidebar = (props) => {
 	);
 
 	return (
-		<Navbar className={`navbar navbar-vertical-aside navbar-vertical navbar-vertical-fixed navbar-expand-xl navbar-bordered navbar-vertical-aside-initialized 
-			${isMobile ? 'd-none' : ''}`}>
+		<Navbar className={`d-none d-md-block navbar navbar-vertical-aside navbar-vertical navbar-vertical-fixed navbar-expand-xl navbar-bordered navbar-vertical-aside-initialized`}>
 			<div className='navbar-vertical-container'>
 				<Navbar.Brand href="/" className='navbar-brand d-flex justify-content-center navbar-brand px-3 mt-3'>
 					<LogoIcon primary={'#377dff'} dark={'#1c232c'} />
