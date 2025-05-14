1a55f8da3e329b9b72f88f0540237c132c348cb6
Moses Surumen <moses.susrumen@gmail.com>
2025-05-14 09:32:32 -0700

    quick menu

--- app/widgets/navbars/QuickMenu.tsx
+++ app/widgets/navbars/QuickMenu.tsx
@@ -14,20 +14,16 @@ const QuickMenu = () => {
     const skin = useSelector((state: RootState) => state.app.skin)
     const dispatch = useDispatch()
     const hasMounted = useMounted()
-    const isDesktop = useMediaQuery({ query: '(min-width: 1224px)' })
 
-    // Persisted storage of the theme
     const { storageValue: storedSkin, setStorageValue } =
         useLocalStorage('skin', skin)
 
-    // Whenever storage changes, update Redux
     useEffect(() => {
         if (storedSkin !== skin) {
             dispatch(changeSkin(storedSkin))
         }
     }, [storedSkin, skin, dispatch])
 
-    // Whenever Redux state changes, write to <html>
     useEffect(() => {
         const root = document.documentElement
         root.setAttribute('data-bs-theme', skin)
@@ -44,20 +40,20 @@ const QuickMenu = () => {
             <Nav.Link
                 target="_blank"
                 href="https://surumen.com"
-                className="nav-item nav-link rounded-pill d-none d-lg-block cursor-pointer"
+                className="nav-item nav-link rounded-pill cursor-pointer"
             >
                 <Person size={22} />
             </Nav.Link>
             <Nav.Link
                 target="_blank"
                 href="https://www.linkedin.com/in/mosessurumen"
-                className="nav-item nav-link rounded-pill d-none d-lg-block cursor-pointer"
+                className="nav-item nav-link rounded-pill cursor-pointer"
             >
                 <Linkedin size={18} />
             </Nav.Link>
             <Nav.Link
                 onClick={toggleTheme}
-                className="nav-item nav-link rounded-pill d-none d-lg-block cursor-pointer"
+                className="nav-item nav-link rounded-pill cursor-pointer"
             >
                 {skin === 'dark' ? <MoonFill size={18} /> : <Sun size={24} />}
             </Nav.Link>
